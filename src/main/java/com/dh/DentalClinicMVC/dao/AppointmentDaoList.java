d093e10391aa2071bdfb7efcb672b08ff9813241
RivaldoSanchezPizango <110652279+RivaldoSanchezPizango@users.noreply.github.com>
2024-10-24 22:07:14 -0300

    Response entity

---
+++ src/main/java/com/dh/DentalClinicMVC/dao/AppointmentDaoList.java
@@ -0,0 +1,56 @@
+package com.dh.DentalClinicMVC.dao;
+
+import com.dh.DentalClinicMVC.model.Appointment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppointmentDaoList implements  IDao<Appointment>{
+
+    private List<Appointment> appointments;
+
+    public AppointmentDaoList() {
+        appointments = new ArrayList<>();
+    }
+
+    @Override
+    public Appointment save(Appointment appointment) {
+        // guardamos en la lista el turno que recibimos por parametro
+        appointments.add(appointment);
+        return appointment;
+    }
+
+    @Override
+    public Appointment findById(Integer id) {
+        Appointment appointmentToLookFor = null;
+        // recorrer la lista con un bucle
+        for (Appointment a : appointments) {
+            // chequeamos que el ID del turno matchee con el ID que estamos buscando
+            if (a.getId() == id) {
+                appointmentToLookFor = a;
+                return appointmentToLookFor;
+            }
+        }
+        return appointmentToLookFor;
+    }
+
+    @Override
+    public void update(Appointment appointment) {
+
+    }
+
+    @Override
+    public void delete(Integer t) {
+
+    }
+
+    @Override
+    public List<Appointment> findAll() {
+        return appointments;
+    }
+
+    @Override
+    public Appointment findByString(String value) {
+        return null;
+    }
+}
