4a9668d9ed738507e4030dc0dc293a472e1ea177
RivaldoSanchezPizango <110652279+RivaldoSanchezPizango@users.noreply.github.com>
2024-11-25 21:52:11 -0300

    configuracion de seguridad, servicio jwt y jwt authenticationFilter

--- src/main/java/com/dh/DentalClinicMVC/configuration/JwtService.java
+++ src/main/java/com/dh/DentalClinicMVC/configuration/JwtService.java
@@ -1,12 +1,39 @@
 package com.dh.DentalClinicMVC.configuration;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
+    private static final String SECRET_KEY = "6aca396da63d5f84ab26dbf18ccbb18713fbd31fd7ca0b93b785c3f2f5b87c28";
+
     public String extractUsername(String token) {
         return null;
+    }
+
+    public <T> T extractClaims(String token, Function<Claims, T> claimsTFunction) {
+        final Claims claims = extractAllClaims(token);
+        return claimsTFunction.apply(claims);
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 
 }
