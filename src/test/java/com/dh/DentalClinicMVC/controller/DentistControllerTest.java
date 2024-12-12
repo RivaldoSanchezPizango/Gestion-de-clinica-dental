cf010e996181c0f780bba33e854f57cc36a8bf5d
RivaldoSanchezPizango <110652279+RivaldoSanchezPizango@users.noreply.github.com>
2024-12-11 22:21:06 -0300

    test de integracion II

--- src/test/java/com/dh/DentalClinicMVC/controller/DentistControllerTest.java
+++ src/test/java/com/dh/DentalClinicMVC/controller/DentistControllerTest.java
@@ -2,8 +2,7 @@ package com.dh.DentalClinicMVC.controller;
 
 import com.dh.DentalClinicMVC.entity.Dentist;
 import com.dh.DentalClinicMVC.service.IDentistService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class DentistControllerTest {
 
     @Autowired
@@ -26,7 +26,6 @@ class DentistControllerTest {
     @Autowired
     private IDentistService iDentistService;
 
-    @BeforeEach
     public void dataLoad(){
         Dentist dentist = new Dentist();
          dentist.setRegistration(2478);
@@ -36,13 +35,39 @@ class DentistControllerTest {
     }
 
     @Test
+    @Order(2)
     public void testGetDentistById() throws Exception {
+        dataLoad();
         mockMvc.perform(get("/odontologos/1")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.registration").value("2478"))
                 .andExpect(jsonPath("$.name").value("Oiler"))
                 .andExpect(jsonPath("$.lastName").value("Sanchez"));
+    }
+
+    @Test
+    @Order(3)
+    public void testPostDentist() throws Exception {
+        String dentistSaved = "{\"registration\": \"1253\",\"name\": \"Lidia\",\"lastName\": \"Pizango\"}";
+
+        mockMvc.perform(post("/odontologos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(dentistSaved)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.registration").value("1253"))
+                .andExpect(jsonPath("$.name").value("Lidia"))
+                .andExpect(jsonPath("$.lastName").value("Pizango"));
+    }
+
+    @Test
+    @Order(1)
+    public void testGetAllDentist() throws Exception {
+        mockMvc.perform(get("/odontologos"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("[]"));
     }
 
 }
